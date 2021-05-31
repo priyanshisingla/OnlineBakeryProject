be4b03efc3ea2bd9970d3ec72a1f2076d7855ebb
Manmeet Kaur <manmeet4818@gmail.com>
2021-05-31 19:48:31 +0530

    committed @manmeet01

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-import { FormControl } from '@angular/forms';
 import { AuthService } from 'src/app/auth.service';
 
 @Component({
