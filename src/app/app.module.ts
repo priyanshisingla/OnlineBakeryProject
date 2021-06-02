465fda46086cd9309f8800def1faf4ac7ce8ab01
Bhavya Garg <gargbhavya0229@gmail.com>
2021-06-02 20:19:47 +0530

    newcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { UserModule } from './user/user.module';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -18,10 +20,12 @@ import { TestimonialsComponent } from './testimonials/testimonials.component';
     TestimonialsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     NgbModule,
     AppRoutingModule,
-    UserModule
+    UserModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
